7168885586128628507f4a8824ffe89d5f7038eb
Thales Aguiar <thalesaguiar21@gmail.com>
2017-12-19 14:16:06 -0300

    Initial structure for Fuzzy Subsets

---
+++ MembershipFunctions/GaussianFuzzySubset.py
@@ -0,0 +1,11 @@
+import FuzzySubset
+
+
+class GaussianFuzzySubset(FuzzySubset):
+    """This class implements a Gaussian distribution Fuzzy Subset"""
+
+    def __init__(self):
+        pass
+
+    def membershipValue(self, value):
+        print('Hey! I\'m a Gaussian Fuzzy Subset')
