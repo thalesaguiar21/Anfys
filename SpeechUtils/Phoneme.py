dcacdfab285aaafef2dda177d734d650c0b36670
Thales Aguiar <thalesaguiar21@gmail.com>
2018-01-06 14:54:15 -0300

    Added SpeechUtils package

---
+++ SpeechUtils/Phoneme.py
@@ -0,0 +1,16 @@
+phonemeList = [
+    'AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'B', 'CH', 'D', 'DH', 'EH', 'ER',
+    'EY', 'F', 'G', 'HH', 'IH', 'IY', 'JH', 'K', 'L', 'M', 'N', 'NG', 'OW',
+    'OY', 'P', 'R', 'S', 'SH', 'T', 'TH', 'UH', 'UW', 'V', 'W', 'Y', 'Z',
+    'ZH'
+]
+
+phonemes = {}
+
+
+def phoneme(i):
+    return phonemeList[i]
+
+
+def index(chars):
+    return phonemes[chars]
---
+++ SpeechUtils/__init__.py
@@ -0,0 +1,4 @@
+from Phoneme import *
+
+for i in range(len(phonemeList)):
+    phonemes[phonemeList[i]] = i
