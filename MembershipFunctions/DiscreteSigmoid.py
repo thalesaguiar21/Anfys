5c89fc64d725e0d983f7efa2b2fa6c7a8ca69d98
Thales Aguiar <thalesaguiar21@gmail.com>
2018-01-06 09:02:54 -0300

    Fixed an mismatch on variable attribuition

--- MembershipFunctions/DiscreteSigmoid.py
+++ MembershipFunctions/DiscreteSigmoid.py
@@ -8,16 +8,15 @@ class DiscreteSigmoid(LinguisticLabel):
     def __init__(self):
         self.a = 1
         self.b = 0
-        pass
 
     def membershipValue(self, value, params):
-        a = 1 / (params[1] - params[0])
-        b = 1 - (params[1] * a)
+        self.a = 1 / (params[1] - params[0])
+        self.b = 1 - (params[1] * self.a)
         result = 0
         if value >= 1:
             result = params[1]
         elif value < 1 and value > 0:
-            result = (value - b) / a
+            result = (value - self.b) / self.a
         else:
             result = params[0]
 
