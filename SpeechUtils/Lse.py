565a608fafa3833b07b579f825d3d958441a9028
Thales Aguiar <thalesaguiar21@gmail.com>
2018-01-08 20:47:02 -0300

    Initial implementation of Least Square Estimation

--- SpeechUtils/Lse.py
+++ SpeechUtils/Lse.py
@@ -1,4 +1,7 @@
-def lse(coefMatrix, rsMatrix):
+from numpy import eye, dot, array, zeros, matrix
+
+
+def lse(coefMatrix, rsMatrix, gamma=0.002):
     """ Computes the Least Square Estimation for the matrices AX = B
 
     Parameters
@@ -7,35 +10,27 @@ def lse(coefMatrix, rsMatrix):
         The coefficient matrix, or A
     rsMatrix : 2D list of double
         The result matrix, or B
+    gamma : double
+        An positive real value
 
     Returns
     -------
-    X : list of double
-        The unknown vector
+    X : numpy array
+        An approximation of X, the unknown vector
     """
-    pass
-
-
-def T(matrix):
-    """ Returns the tranpose of a matrix
-
-    Parameters
-    ----------
-    matrix : list
-        The original matrix
-
-    Returns
-    -------
-    matrixT : list
-        The transpose of matrix
-    """
-    if type(matrix[0]) is not []:
-        return matrix
-    else:
-        matrixT = [
-            [0 for j in range(len(matrix))] for i in range(len(matrix[0]))
-        ]
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                matrixT[j][i] = matrix[i][j]
-        return matrixT
+    A = array(coefMatrix)
+    S = eye(len(coefMatrix))
+    X = zeros(A.shape[1], 1)
+    for i in range(len(A[:, 0])):
+        a_i = A[i, :]
+        b_i = array(rsMatrix[i])
+        S = S - (
+            array(
+                dot(dot(dot(S, matrix(a_i).T), matrix(a_i)), S))) \
+            / (gamma + (dot(dot(S, a_i), a_i)))
+        S *= 1 / gamma
+        X = X + (dot(
+            S,
+            dot(matrix(a_i).T, (matrix(b_i) - dot(matrix(a_i), X))))
+        )
+    return X
