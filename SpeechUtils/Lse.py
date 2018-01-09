c32bb50d699f78e1457f9159d46a35ffb67ee417
Thales Aguiar <thalesaguiar21@gmail.com>
2018-01-09 18:41:05 -0300

    Fixed LSE to work with multi-dim constant matrix

--- SpeechUtils/Lse.py
+++ SpeechUtils/Lse.py
@@ -1,7 +1,7 @@
 from numpy import eye, dot, array, zeros, matrix
 
 
-def lse(coefMatrix, rsMatrix, gamma=0.002):
+def lse(coefMatrix, rsMatrix, lamb=0.2):
     """ Computes the Least Square Estimation for the matrices AX = B
 
     Parameters
@@ -10,7 +10,7 @@ def lse(coefMatrix, rsMatrix, gamma=0.002):
         The coefficient matrix, or A
     rsMatrix : 2D list of double
         The result matrix, or B
-    gamma : double
+    lamb : double
         An positive real value
 
     Returns
@@ -19,16 +19,16 @@ def lse(coefMatrix, rsMatrix, gamma=0.002):
         An approximation of X, the unknown vector
     """
     A = array(coefMatrix)
-    S = eye(len(coefMatrix))
-    X = zeros(A.shape[1], 1)
+    S = eye(len(coefMatrix[0]))
+    X = zeros((A.shape[1], A.shape[0]))
     for i in range(len(A[:, 0])):
         a_i = A[i, :]
         b_i = array(rsMatrix[i])
         S = S - (
             array(
                 dot(dot(dot(S, matrix(a_i).T), matrix(a_i)), S))) \
-            / (gamma + (dot(dot(S, a_i), a_i)))
-        S *= 1 / gamma
+            / (lamb + (dot(dot(S, a_i), a_i)))
+        S *= 1 / lamb
         X = X + (dot(
             S,
             dot(matrix(a_i).T, (matrix(b_i) - dot(matrix(a_i), X))))
