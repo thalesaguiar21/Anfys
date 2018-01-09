f82d37cc9b2a710b4db10e16aec8c90d791bf28b
Thales Aguiar <thalesaguiar21@gmail.com>
2018-01-09 19:37:22 -0300

    Added step functions for each phoneme, as well as the threshold function

--- SpeechUtils/Phoneme.py
+++ SpeechUtils/Phoneme.py
@@ -1,9 +1,16 @@
+from random import random
+
+
 phonemeList = [
     'AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'B', 'CH', 'D', 'DH', 'EH', 'ER',
     'EY', 'F', 'G', 'HH', 'IH', 'IY', 'JH', 'K', 'L', 'M', 'N', 'NG', 'OW',
     'OY', 'P', 'R', 'S', 'SH', 'T', 'TH', 'UH', 'UW', 'V', 'W', 'Y', 'Z',
     'ZH'
 ]
+
+thresholds = [random()] * len(phonemeList)
+for i in range(1, len(thresholds)):
+    thresholds[i] = thresholds[i - 1] + random()
 
 phonemes = {}
 
@@ -46,3 +53,41 @@ def index(chars):
     KeyError -- if the given phoneme is not in the dictionary.
     """
     return phonemes[chars]
+
+
+def step(x):
+    """ Computes if the given value correponds to any phoneme
+
+    Parameters
+    ----------
+    x : double
+        A value to be tested
+
+    Returns
+    -------
+    phn : str
+        An string with the corresponding phoneme, or '' if the value does not
+        correspond to any phoneme.
+    """
+    phn = ''
+    for i in range(len(thresholds)):
+        if x <= thresholds[i]:
+            phn = phonemeList[i]
+            break
+    return phn
+
+
+def phone_threshold(phone):
+    """ Gets the threshold value for an phoneme
+
+    Parameters
+    ----------
+    phone : str
+        A string with an representation of the phoneme
+
+    Returns
+    -------
+    threshold : double
+        The maximum value where the given phoneme would be inferred
+    """
+    return thresholds[index(phone)]
