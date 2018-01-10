7999496320af4d4dafd57ec34b7865f04c804d6c
Thales Aguiar <thalesaguiar21@gmail.com>
2018-01-10 10:51:45 -0300

    Added log_print documentation

--- Errors/Debug.py
+++ Errors/Debug.py
@@ -3,6 +3,15 @@ DEBUG = True
 
 
 def log_print(msg, end_=None):
+    """ Only prints the given message if the DEBUG mode is True
+
+    Paramters
+    ---------
+    msg : str
+        The message
+    end_ : str
+        This will be passed to print as its 'end' argument
+    """
     if DEBUG is True and end_ is not None:
         print(msg, end=end_)
     elif DEBUG is True:
