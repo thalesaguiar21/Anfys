57e62214443e455e716f84b5126dcf72cf6f4f4f
Thales Aguiar <thalesaguiar21@gmail.com>
2018-04-03 11:07:15 -0300

    Removed MembershipFunctions package

--- MembershipFunctions/__init__.py
+++
@@ -1,6 +0,0 @@
-from DiscreteSigmoid import DiscreteSigmoid
-from Logit import Logit
-from FuzzySubset import FuzzySubset
-from GaussianThree import GaussianThree
-from GaussianTwo import GaussianTwo
-from LinguisticLabel import LinguisticLabel
