2b8e70fc994fee6c960996fe9bb9cda52df93b50
Thales Aguiar <thalesaguiar21@gmail.com>
2018-04-06 20:28:08 -0300

    Added test cases for _create_rules function

--- tests/tests_speech_anfis.py
+++ tests/tests_speech_anfis.py
@@ -3,4 +3,39 @@ import unittest2 as unittest
 
 
 class TestAnfis(unittest.TestCase):
-    pass
+
+    def __init__(self):
+        self.outputs = [[1, 2, 3], [2, 2], [0.5, 3]]
+        self.rules_size = [3, 2, 2]
+
+    def test_rules(self):
+        expected = [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0),
+                    (1, 0, 1), (1, 1, 0), (1, 1, 1), (2, 0, 0), (2, 0, 1),
+                    (2, 1, 0), (2, 1, 1)]
+        res = anfis._create_rules(self.rules_size)
+        self.assertSequenceEqual(res, expected)
+
+    def test_rules_none(self):
+        try:
+            anfis._create_rules(None)
+            self.fail('Rules created with no sets!')
+        except ValueError:
+            pass
+
+    def test_rules_one(self):
+        self.rules_size = [2]
+        res = anfis._create_rules(self.rules_size)
+        self.assertSequenceEqual([(0,), (1,)], res)
+
+    def test_rules_empty(self):
+        self.assertSequenceEqual([()], anfis._create_rules([]))
+
+    def run_all(self):
+        self.test_rules()
+        self.test_rules_none()
+        self.test_rules_one()
+        self.test_rules_empty()
+
+
+test_anfis = TestAnfis()
+test_anfis.run_all()
