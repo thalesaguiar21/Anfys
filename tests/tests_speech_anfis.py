4ad2a6b1ce6c7a815351d02f11f69330261e71dd
Thales Aguiar <thalesaguiar21@gmail.com>
2018-04-06 23:04:49 -0300

    Moved anfis module function tests to only one test case

--- tests/tests_speech_anfis.py
+++ tests/tests_speech_anfis.py
@@ -2,40 +2,89 @@ from context import anfis
 import unittest2 as unittest
 
 
-class TestRuleCreation(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
 
     def __init__(self):
         self.outputs = [[1, 2, 3], [2, 2], [0.5, 3]]
-        self.rules_size = [3, 2, 2]
+        self.anfis = anfis.BaseModel([3, 2, 2])
+
+    def setup(self):
+        self.outputs = [[1, 2, 3], [2, 2], [0.5, 3]]
+        self.anfis = anfis.BaseModel([3, 2, 2])
 
     def test_rules(self):
         expected = [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0),
                     (1, 0, 1), (1, 1, 0), (1, 1, 1), (2, 0, 0), (2, 0, 1),
                     (2, 1, 0), (2, 1, 1)]
-        res = anfis._create_rules(self.rules_size)
-        self.assertSequenceEqual(res, expected)
+        self.assertSequenceEqual(expected, self.anfis.rule_set)
 
     def test_rules_none(self):
         try:
-            anfis._create_rules(None)
+            self.anfis = anfis.BaseModel(None)
+            self.fail('Rules created with no sets!')
+        except ValueError:
+            pass
+
+        try:
+            self.anfis = anfis.BaseModel([-1, 2, 3])
+            self.fail('Rules created with negative size!')
+        except ValueError:
+            pass
+
+        try:
+            self.anfis = anfis.BaseModel([-1, 2, None])
             self.fail('Rules created with no sets!')
         except ValueError:
             pass
 
     def test_rules_one(self):
-        self.rules_size = [2]
-        res = anfis._create_rules(self.rules_size)
-        self.assertSequenceEqual([(0,), (1,)], res)
+        self.anfis = anfis.BaseModel([2])
+        self.assertSequenceEqual([(0,), (1,)], self.anfis.rule_set)
 
     def test_rules_empty(self):
-        self.assertSequenceEqual([()], anfis._create_rules([]))
+        self.anfis = anfis.BaseModel([])
+        self.assertSequenceEqual([()], self.anfis.rule_set)
+
+    def test_min_expected(self):
+        self.setup()
+        expected = [0.5, 1, 0.5, 1, 0.5, 2, 0.5, 2, 0.5, 2, 0.5, 2]
+        self.assertSequenceEqual(
+            expected, self.anfis._min_operation(self.outputs))
+
+    def test_min_no_output(self):
+        self.setup()
+        try:
+            self.anfis._min_operation(None)
+            self.fail('Computed min operation with None as outputs!')
+        except ValueError:
+            pass
+
+    def test_prod_expected(self):
+        self.setup()
+        expected = [1.0, 6.0, 1.0, 6.0, 2.0, 12.0,
+                    2.0, 12.0, 3.0, 18.0, 3.0, 18.0]
+        self.assertSequenceEqual(
+            expected, self.anfis._product_operation(self.outputs))
+
+    def test_prod_no_output(self):
+        self.setup()
+        try:
+            self.anfis._product_operation(None)
+            self.fail('Computed prod operation with None as outputs!')
+        except ValueError:
+            pass
 
     def run_all(self):
         self.test_rules()
         self.test_rules_none()
         self.test_rules_one()
         self.test_rules_empty()
+        self.test_min_expected()
+        self.test_min_no_output()
+        self.test_prod_expected()
+        self.test_prod_no_output()
 
 
-test_anfis = TestRuleCreation()
-test_anfis.run_all()
+test_base_model = TestBaseModel()
+
+test_base_model.run_all()
