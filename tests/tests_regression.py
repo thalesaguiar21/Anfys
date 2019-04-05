4e1f0b14af52ef75b15f529635a11bfa49d0493e
Thales Aguiar <thalesaguiar21@gmail.com>
2019-04-04 22:13:09 -0300

    Removed docs from test function for regresion module

--- tests/tests_regression.py
+++ tests/tests_regression.py
@@ -13,13 +13,6 @@ class TestUtils(unittest.TestCase):
             self.assertAlmostEqual(expected, curr[0])
 
     def test_lse_offline(self):
-        """ Test for lse offline with the following linear system
-
-        2x + 3y = 5
-        4x -  y = -1
-
-        The result is x = 0.1428571428571429 and y = 1.571428571428571
-        """
         coef_matrix = [[2, 3], [4, -1]]
         rs_matrix = [5, -1]
         expected = [0.1428571428571429, 1.571428571428571]
