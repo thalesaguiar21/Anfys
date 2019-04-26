02f7365b23faf5e9bc353b9ba1e1898056e464d7
Thales Aguiar <thalesaguiar21@gmail.com>
2019-04-26 20:26:57 -0300

    Generalised test suite loader

--- run_tests.py
+++ run_tests.py
@@ -4,18 +4,11 @@ import tests.tests_regression as tests_regression
 import tests.tests_tnorm as tests_tnorm
 import tests.tests_tconorm as tests_tconorm
 
+test_units = [tests_mem_funcs,
+              tests_regression,
+              tests_tnorm,
+              tests_tconorm]
 
-suite_funcs = unittest.TestLoader().loadTestsFromModule(tests_mem_funcs)
-unittest.TextTestRunner(verbosity=2).run(suite_funcs)
-
-suite_reg = unittest.TestLoader().loadTestsFromModule(tests_regression)
-unittest.TextTestRunner(verbosity=2).run(suite_reg)
-
-suite_tnorm = unittest.TestLoader().loadTestsFromModule(tests_tnorm)
-unittest.TextTestRunner(verbosity=2).run(suite_tnorm)
-
-suite_tconorm = unittest.TestLoader().loadTestsFromModule(tests_tconorm)
-unittest.TextTestRunner(verbosity=2).run(suite_tconorm)
-
-# suite_anfis = unittest.TestLoader().loadTestsFromModule(tests_speech_anfis)
-# unittest.TextTestRunner(verbosity=2).run(suite_anfis)
+for utest in test_units:
+    suite = unittest.TestLoader().loadTestsFromModule(utest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
