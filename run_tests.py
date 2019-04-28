9f931e7ca21f54b2c0597e9907cda32c292c79aa
Thales Aguiar <thalesaguiar21@gmail.com>
2019-04-28 06:02:54 -0300

    Fixed and improved clipping tests Fixed calling to assertSequenceAlmostEqual for lse testing methods

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,15 @@
 import unittest
 import tests.tests_mem_funcs as tests_mem_funcs
-import tests.tests_regression as tests_regression
+import tests.tests_lse as tests_lse
 import tests.tests_tnorm as tests_tnorm
 import tests.tests_tconorm as tests_tconorm
 
 # test_units = [tests_mem_funcs,
-#               tests_regression,
+#               tests_lse,
 #               tests_tnorm,
 #               tests_tconorm]
 
-test_units = [tests_regression]
+test_units = [tests_lse]
 
 for utest in test_units:
     suite = unittest.TestLoader().loadTestsFromModule(utest)
--- tests/tests_lse.py
+++ tests/tests_lse.py
@@ -10,14 +10,14 @@ class TestMatrix(unittest.TestCase):
         rs_matrix = np.array([2, 4, 8])
         expected = [23 / 7, 8 / 7]
         res = lse.Matricial().solve(coef_matrix, rs_matrix)
-        self.assertSequenceAlmostEqual(self, expected, res)
+        assertSequenceAlmostEqual(self, expected, res)
 
     def test_matricial_determined(self):
         coef_matrix = np.array([[2, 3], [4, -1]])
         rs_matrix = np.array([5, -1])
         expected = [0.1428571428571429, 1.571428571428571]
         res = lse.Matricial().solve(coef_matrix, rs_matrix)
-        self.assertSequenceAlmostEqual(self, expected, res)
+        assertSequenceAlmostEqual(self, expected, res)
 
     def test_matricial_underdetermined(self):
         pass
@@ -30,14 +30,14 @@ class TestRecursive(unittest.TestCase):
         rs_matrix = np.array([12, 13, 11])
         expected = [12.002992517583628, 12.998997523491125, 10.996013446352867]
         res = lse.Recursive(1.0, 1000).solve(coef_matrix, rs_matrix)
-        self.assertSequenceAlmostEqual(self, expected, res)
+        assertSequenceAlmostEqual(self, expected, res)
 
     def test_recursive_overdetermined(self):
         coef_matrix = np.array([[1, -1], [1, 1], [2, 1]])
         rs_matrix = np.array([2, 4, 8])
         expected = [23 / 7, 8 / 7]
         res = lse.Recursive(1.0, 1000).solve(coef_matrix, rs_matrix)
-        self.assertSequenceAlmostEqual(self, expected, res)
+        assertSequenceAlmostEqual(self, expected, res)
 
 def assertSequenceAlmostEqual(testcase, seq1, seq2, tolerance=7):
     for s1, s2 in zip(seq1, seq2):
@@ -52,28 +52,31 @@ class TestClipping(unittest.TestCase):
         self.upper = upper
 
     def do_clip(self):
-        return lse.clip(self.value, self.lower, self.upper)
+        self.clippedvalue = lse.clip(self.value, self.lower, self.upper)
+
+    def cliped_result_is(self, expected):
+        self.assertAlmostEqual(self.clippedvalue, expected)
 
     def test_clipping_inside(self):
-        when_values_are(2, 1, 100)
-        clippedvalue = self.do_clip()
-        self.assertEqual(clippedvalue, value)
+        self.when_values_are(2, 1, 100)
+        self.do_clip()
+        self.cliped_result_is(2)
 
     def test_clipping_outside_max(self):
-        when_values_are(1000, 1, 100)
-        clippedvalue = self.do_clip()
-        self.assertEqual(clippedvalue, maxvalue)
+        self.when_values_are(1000, 1, 100)
+        self.do_clip()
+        self.cliped_result_is(100)
 
     def test_clipping_close_max(self):
-        when_values_are(100 - 1e-10, 1, 100)
-        clippedvalue = self.do_clip()
+        self.when_values_are(100 - 1e-10, 1, 100)
+        self.do_clip()
 
     def test_clipping_outside_min(self):
-        when_values_are(-10, 1, 100)
-        clippedvalue = self.do_clip()
-        self.assertEqual(clippedvalue, minvalue)
+        self.when_values_are(-10, 1, 100)
+        self.do_clip()
+        self.cliped_result_is(1)
 
     def test_clipping_close_min(self):
-        when_values_are(1 - 1e-10, 1, 100)
-        clippedvalue = self.do_clip()
-        self.assertEqual(clippedvalue, minvalue)
+        self.when_values_are(1 - 1e-10, 1, 100)
+        self.do_clip()
+        self.cliped_result_is(1)
