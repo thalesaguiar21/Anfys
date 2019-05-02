09f40f567ba8470af58d2170856f31772e19e5df
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-02 09:18:46 -0300

    Changed test values

--- tests/tests_neural.py
+++ tests/tests_neural.py
@@ -8,5 +8,5 @@ import numpy as np
 class TestTsukamoto(unittest.TestCase):
 
     def test_forward_pass(self):
-        model = anfis.Tsukamoto(3, mfs.BellTwo())
-        model.hybrid_learn(np.zeros((10, 2)), np.zeros(10), 2)
+        model = anfis.Tsukamoto(2, mfs.BellTwo())
+        model.hybrid_learn(np.zeros((10, 3)), np.zeros(10), 2)
