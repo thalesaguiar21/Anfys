e293d053e5f284c3fbc20b393ba84ed909ad1f70
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-06 19:14:54 -0300

    Removed Tsukamoto model implementation

--- tests/tests_neural.py
+++ tests/tests_neural.py
@@ -4,17 +4,6 @@ import unittest
 import numpy as np
 
 
-class TestTsukamoto(unittest.TestCase):
-
-    def when_model_mfs_is(self, qtd_mf):
-        self.model = anfis.Tsukamoto(qtd_mf)
-
-    def hybrid_learn_for_data_shape(self, nsamp, nfeat):
-        inputs = np.zeros((nsamp, nfeat))
-        out = np.zeros(nsamp)
-        return self.model.hybrid_learn(inputs, out, 1)
-
-
 class TestSugeno(unittest.TestCase):
 
     def when_model_qtd_of_mf_is(self, qtd_mf):
