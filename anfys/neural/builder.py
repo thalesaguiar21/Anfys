0f90379bd609227f5496dc79a4f9d0e243c802a8
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-06 23:23:22 -0300

    Added qtd_inputs attribute initialisation

--- anfys/neural/builder.py
+++ anfys/neural/builder.py
@@ -4,6 +4,7 @@ from anfys.fuzzy.subsets import FuzzySet
 
 def configure_model(anfis, qtd_inputs, stdev=1.0):
     anfis.qtd_rules = qtd_inputs ** anfis.subset_size
+    anfis.qtd_inputs = qtd_inputs
     _build_subsets(anfis, qtd_inputs)
     _build_prem_params(anfis, stdev)
     _initialise_cons_params(anfis, qtd_inputs)
